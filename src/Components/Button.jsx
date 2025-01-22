f774b36e2200fda61777bfb7e7942473ad9fd225
Aholu Samuel <samuelaholu15@gmail.com>
2025-01-22 09:44:28 +0100

    Update

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const Button = () => {
   return (
-    <button className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 bg-green-button-gradient text-white rounded-[14px] border-0 p-4 text-black text-xl">
-      Pre-order Now
-    </button>
+    <a href="https://selar.co/fj932q" target="_blank">
+      <button className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 bg-green-button-gradient text-white rounded-[14px] border-0 p-4 text-black text-xl">
+        Pre-order Now
+      </button>
+    </a>
   );
 };
 
